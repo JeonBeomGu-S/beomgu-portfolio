536af5355c87bf63d88a0cdf4851d7d0cc23d9b8
Beomgu <jb98967@gmail.com>
2025-02-01 16:14:01 -0500

    [Fix] Fix the deploy error with reference document error

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import Lottie from 'react-lottie-player';
+// import Lottie from 'react-lottie-player';
+import dynamic from 'next/dynamic';
+
+const LottiePlayer = dynamic(() => import('react-lottie-player'), {
+  ssr: false, // 서버 측 렌더링을 비활성화
+});
 
 import lottieJson from '/public/main-animation.json';
 
 export default function Example() {
-  return <Lottie loop animationData={lottieJson} play />;
+  return <LottiePlayer loop animationData={lottieJson} play />;
 }
