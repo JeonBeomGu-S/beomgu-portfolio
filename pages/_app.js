d4f79d64d48499ed7f92b3feb839780353bb091c
Beomgu <jb98967@gmail.com>
2025-02-01 15:44:11 -0500

    [Fix] Fix the deploy error with reference document error

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Beomgu&apos;s Portfolio</title>
+        <meta name="description" content="Beomgu's Portfolio"></meta>
+      </Head>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Beomgu&apos;s Portfolio</title>
-      <meta name="description" content="Beomgu's Portfolio"></meta>
       <Head />
       <body className="antialiased">
         <Main />
