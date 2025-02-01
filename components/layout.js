cd8c0263cc020a236f1a26bd615d123ed740bfc3
Beomgu <jb98967@gmail.com>
2025-02-01 15:57:11 -0500

    [Fix] Fix the deploy error with reference document error

--- components/layout.js
+++ components/layout.js
@@ -4,16 +4,10 @@ import Footer from './footer';
 
 export default function Layout({ children }) {
   return (
-    <>
-      <Head>
-        <title>Beomgu&apos;s Portfolio</title>
-        <meta name="description" content="Beomgu's Portfolio"></meta>
-      </Head>
-      <div className="bg-primary">
-        <Header />
-        <div>{children}</div>
-        <Footer />
-      </div>
-    </>
+    <div className="bg-primary">
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </div>
   );
 }
