493100d994572396fce3a2c80f127aa5d2311025
Beomgu <jb98967@gmail.com>
2025-02-01 16:57:07 -0500

    [Feat] Update the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,22 @@
 export default function PageNotFound() {
   return (
     <>
-      <h1>Page Not Found</h1>
+      <div className="flex justify-center items-center bg-primary h-screen">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl">404</h1>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+              Something's missing.
+            </p>
+            <p className="mb-4 text-lg font-light">
+              Sorry, we can't find that page. You'll find lots to explore on the home page.{' '}
+            </p>
+            <a href="/" type="button" className="btn-backhome">
+              Back to Homepage
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
