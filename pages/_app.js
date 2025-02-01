6393a268f3c2f79db2ab7943549ec679d9abce6b
Beomgu <jb98967@gmail.com>
2025-02-01 15:55:17 -0500

    [Fix] Fix the deploy error with reference document error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
