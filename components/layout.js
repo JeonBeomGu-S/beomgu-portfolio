ce9a9ec15f935150e0ef161192ba1520bdc437f7
Beomgu <jb98967@gmail.com>
2025-02-01 15:47:14 -0500

    [Fix] Fix the deploy error with reference document error

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,19 @@
+import Head from 'next/head';
 import Header from './header';
 import Footer from './footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-primary">
-      <Header />
-      <div>{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Beomgu&apos;s Portfolio</title>
+        <meta name="description" content="Beomgu's Portfolio"></meta>
+      </Head>
+      <div className="bg-primary">
+        <Header />
+        <div>{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,10 @@
 import '@/styles/globals.css';
-import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Beomgu&apos;s Portfolio</title>
-        <meta name="description" content="Beomgu's Portfolio"></meta>
-      </Head>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
