aa7633620b438c6bb9206e05a1ddde07ff9da112
Beomgu <jb98967@gmail.com>
2025-02-01 14:48:02 -0500

    [Chore] Change the home section alignment

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Hero from '../components/home/hero';
 export default function Home() {
   return (
     <Layout>
-      <section className="text-gray-600 body-font">
+      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
