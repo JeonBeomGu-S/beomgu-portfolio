b80fbf47b714fd905c3307594864da2c5b9e0b15
Beomgu <jb98967@gmail.com>
2025-02-02 00:21:27 -0500

    [Feat] Add SEO tags

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="en">
       <title>Beomgu&apos;s Portfolio</title>
       <meta name="description" content="Beomgu's Portfolio"></meta>
+
+      {/* Open Graph (OG) Tags */}
+      <meta property="og:title" content="Beomgu's Portfolio" />
+      <meta property="og:description" content="This is Beomgu's Portfolio using Next.js 15" />
+      <meta property="og:image" content="https://beomgu-portfolio.vercel.app/preview.png" />
+      <meta property="og:url" content="https://beomgu-portfolio.vercel.app/" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Beomgu's Portfolio" />
+      <meta name="twitter:description" content="This is Beomgu's Portfolio using Next.js 15" />
+      <meta name="twitter:image" content="https://beomgu-portfolio.vercel.app/preview.png" />
+
       <Head />
       <body className="antialiased">
         <Main />
